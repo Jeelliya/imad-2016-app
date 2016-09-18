71076b3947083466fd64626d285c18f294e7864e
Jeelliya <devilmanojprasad@gmail.com>
2016-09-18 08:53:37 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.get('/article-1',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-1.html'));
 });
 
-app.get('/artical-2',function(req,res){
+app.get('/article-2',function(req,res){
     
      res.sendFile(path.join(__dirname, 'ui', 'article-2.html'));
 });
 
-app.get('/artical-3',function(req,res){
+app.get('/article-3',function(req,res){
     
     res.sendFile(path.join(__dirname, 'ui', 'article-3.html'));
 });
