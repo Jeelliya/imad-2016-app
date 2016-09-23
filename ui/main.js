e39ee80b287345949ca67239e544de8bdeb3924d
Jeelliya <devilmanojprasad@gmail.com>
2016-09-23 22:14:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,8 @@
 console.log('Loaded!');
 prompt("hello i am manoj and is learning the IMAD  online cources ");
+// changing the contents using javascript
+
+var element = document.getdocumentbyid("main-text"
+);
+
+element.converthtml = "new value";
