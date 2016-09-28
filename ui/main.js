22787a25b50cf87e1ee6bf7e0d979b24d509881d
Jeelliya <devilmanojprasad@gmail.com>
2016-09-28 23:20:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,1 @@
-console.log('Loaded!');
-prompt("hello i am manoj and is learning the IMAD  online cources ");
-// changing the contents using javascript
-
-var element = document.getElementById("main-text"
-);
-
-element.innerHTML = "new value";
-
-// changing the image on click 
-
-var img = document.getElementById("madi"
-);
-
- img.onclick = function(){
-     
-     img.style.marginLeft = '100px';
- };
+var button = document.getElementById("counter");
